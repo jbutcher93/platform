7ca98d5766219369c83583c1cd54f85e56e1a06f
jbutcher93 <1butcherjohn@gmail.com>
2023-11-22 15:10:10 -0500

    separated out test

--- infrastructure/terraform/workspaces/cluster/providers.tf
+++ infrastructure/terraform/workspaces/cluster/providers.tf
@@ -1,15 +1,3 @@
 provider "kubectl" {
   load_config_file = true
 }
-
-provider "google" {
-  project = "buoyant-arena-405622"
-  region  = "us-central1"
-  zone    = "us-central1-a"
-}
-
-provider "google-beta" {
-  project = "buoyant-arena-405622"
-  region  = "us-central1"
-  zone    = "us-central1-a"
-}
